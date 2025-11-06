47ac13c6549cc3f97fa40ddb399dbe558f1b20a0
VictorPLima <vicperlim@gmail.com>
2025-11-06 20:58:36 -0300

    Definição de nós de checkpoint e inicialização dos pesos das arestas

--- randomized_perfect_maze.py
+++ randomized_perfect_maze.py
@@ -3,39 +3,42 @@ import matplotlib.pyplot as plt
 import random
 
 
+### Geração de labirinto
+
 def create_perfect_maze_from_MST(G, algorithm):
     """
-    Cria grafo com pesos aleatórios para consequentemente, ao produzir a 
-    árvore geradora mínima por Kruskal/Prim/Boruvka, produzir um labirinto 
+    Cria grafo com pesos aleatórios para consequentemente, ao produzir a
+    árvore geradora mínima por Kruskal/Prim/Boruvka, produzir um labirinto
     perfeito (sem ciclos).
     """
     for u, v in G.edges():
         G.edges[u, v]['weight'] = round(random.uniform(1.0, 10.0), 1)
 
     random_tree = nx.minimum_spanning_tree(G, algorithm=algorithm)
-    
+
     return random_tree
 
 
 # Definindo o grafo grid a partir do qual produziremos o labirinto
-maze_shape = [4, 4]
-G = nx.grid_graph(maze_shape)
+m, n = [5, 4]
+G = nx.grid_graph([m, n])
 
 MST_algorithm = 'kruskal'
 T = create_perfect_maze_from_MST(G, MST_algorithm)
 
-pos = dict((n, n) for n in G.nodes()) # Usa a posição na grid como layout
+pos_grid = dict((n, n) for n in G.nodes()) # Usa a posição na grid como layout
+pos_spring = nx.spring_layout(G)
 edge_labels = nx.get_edge_attributes(G, 'weight')
 
 # Mostrando os resultados
 plt.figure(figsize=(15, 5))
 plt.subplot(131)
-nx.draw(G, pos, with_labels=False, node_color='skyblue', node_size=500, edge_color='gray')
-nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red')
+nx.draw(G, pos_grid, with_labels=True, node_color='skyblue', node_size=500, edge_color='gray', font_size=9)
+nx.draw_networkx_edge_labels(G, pos_grid, edge_labels=edge_labels, font_color='red', font_size=9)
 plt.title("G - Original Graph (random weights)\n")
 
 plt.subplot(132)
-nx.draw(T, pos, with_labels=False, node_color='skyblue', node_size=500, edge_color='black')
+nx.draw(T, pos_grid, with_labels=True, node_color='skyblue', node_size=500, edge_color='black', font_size=9)
 plt.title("T - Random Spanning Tree ("+MST_algorithm+")\n (Random Maze)")
 
 edges_to_remove = T.edges()
@@ -43,7 +46,49 @@ M = G.copy()
 M.remove_edges_from(edges_to_remove)
 
 plt.subplot(133)
-nx.draw(M, pos, with_labels=False, node_color='skyblue', node_size=500, edge_color='black')
+nx.draw(M, pos_grid, with_labels=False, node_color='skyblue', node_size=500, edge_color='black')
 plt.title("G-T\n (Maze walls)")
+
+plt.show()
+
+
+
+### Definindo os custos de cada aresta para o jogo
+
+maze = T.copy()
+
+start, end = (0,0), (n-1,m-1)
+# talvez seja mais interessante se o start e end nodes sejam os nós folha 
+# mais distantes entre si, o que pode ser descoberto fazendo BFS
+
+V = list(maze.nodes())
+E = list(maze.edges())
+checkpoint_nodes = [node for node in V if node not in [start, end] and maze.degree(node)==1]
+# checkpoint nodes são todos os nós folha da árvore geradora, exceto os de start e end.
+# Podemos vê-los também como o final de becos sem saída do labirinto
+
+print("V:")
+print(V)
+print("Checkpoint nodes:")
+print(checkpoint_nodes)
+
+for u, v in maze.edges():
+  if(v in checkpoint_nodes or u in checkpoint_nodes):
+    maze.edges[u, v]['weight'] = +3 
+    # definir isso de forma automatizada colocando pesos (recompensas de vida) 
+    # que façam o jogador questionar se vale a pena ou não entrar em algum 
+    # checkpoint (beco sem saída) para aumentar sua pontuação de vida
+  else:
+    maze.edges[u, v]['weight'] = -1
+
+
+maze_edge_labels = nx.get_edge_attributes(maze, 'weight')
+
+# Plotando labirinto
+plt.figure(figsize=(5, 5))
+plt.title("Maze with game weights\n")
+nx.draw(maze, pos_grid, with_labels=True, node_color='skyblue', node_size=500, edge_color='gray', font_size=9)
+nx.draw_networkx_edge_labels(maze, pos_grid, edge_labels=maze_edge_labels, font_color='red', font_size=9)
+
 plt.show()
 
