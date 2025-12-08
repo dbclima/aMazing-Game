e0ff84f700d8a85cca9dfeaa72682a096b49772a
jvmagnobf <jvmagnob105@gmail.com>
2025-12-08 20:24:30 -0300

    Funcoes de salvar e pegar as stats

--- src/database/crud.py
+++ src/database/crud.py
@@ -2,8 +2,9 @@ import json
 import networkx as nx
 from labirinto import Labirinto
 from labirinto import criar_labirinto
+from labirinto import Dificuldade
 
-def saveLab(L: Labirinto, id: int):
+def saveLab(L: Labirinto):
     labirinto = {}
     arestas = []
     for i, j in L.grafo.edges:
@@ -22,10 +23,12 @@ def saveLab(L: Labirinto, id: int):
     labirinto["origem"] = origem
     labirinto["chegada"] = chegada
     labirinto["checkpoints"] = checkpoints
+    labirinto["vida inicial"] = L.vida_inicial
+    labirinto["dificuldade"] = L.dificuldade.value
     
     with open("fases.json", "r") as arquivo:
         antigo = json.load(arquivo)
-        antigo[id] = labirinto
+        antigo[L.id] = labirinto
     
     with open("fases.json", "w") as arquivo:
         json.dump(antigo, arquivo)
@@ -43,6 +46,12 @@ def loadLab(id: int):
     for i in aux_check.keys():
         checkpoints[tuple(map(int, i.split(",")))] = aux_check[i]
     
+    if labirinto["dificuldade"] == 1:
+        dificuldade = Dificuldade.DIFICIL
+    else:
+        dificuldade = Dificuldade.FACIL
+    
+    vida_inicial = labirinto["vida inicial"]
     arestas = []
     aux_arestas = labirinto["arestas"]
     for i, j in aux_arestas:
@@ -51,13 +60,50 @@ def loadLab(id: int):
     grafo = nx.Graph()
     for i, j in arestas:
         grafo.add_edge(i, j)
-    return Labirinto(grafo, dimensoes, origem, chegada, checkpoints)
+    return Labirinto(id, grafo, dimensoes, origem, chegada, checkpoints, vida_inicial, dificuldade)
 
+def saveStats(id: int, username: str, score: int, time: float):
+    stats = {}
+    
+    stats["score"] = score
+    stats["time"] = time
+    
+    id_str = str(id)
+    with open("leaderboard.json", "r") as arquivo:
+        antigo = json.load(arquivo)
+        if id_str not in antigo.keys():
+            antigo[id_str] = dict()
+        antigo[id_str][username] = stats
+    
+    with open("leaderboard.json", "w") as arquivo:
+        json.dump(antigo, arquivo)
+
+def getStats(id: int):
+    id_str = str(id)
+    
+    with open("leaderboard.json", "r") as arquivo:
+        antigo = json.load(arquivo)
+        leaderboard = antigo[id_str]
+    
+    leaderboard_ordenado = sorted(leaderboard.items(), key=lambda x: (-x[1]["score"], x[1]["time"]))
+    output = leaderboard_ordenado
+    if len(leaderboard_ordenado) >10:
+        output = leaderboard_ordenado[:10]
+
+    return output
 
 if __name__ == "__main__":
-    lab = criar_labirinto(3, 3)
-    saveLab(lab, 0)
-    print(lab)
-    lab1 = loadLab("0")
-    print(lab1)
-    
+    #saveStats(0, "didi", 20, 10.5)
+    #saveStats(0, "tonhao", 20, 14)
+    #saveStats(0, "giovanna", 25, 20)
+    #saveStats(0, "joao", 12, 19)
+    #saveStats(0, "arrasca", 33, 200)
+    #saveStats(0, "mann", 33, 199.9)
+    #saveStats(0, "bh", 25, 20.9)
+    #saveStats(0, "ortiz", 19, 23)
+    #saveStats(0, "gabigol", 19, 99)
+    #saveStats(0, "er", 19, 7)
+    #saveStats(0, "fili", 19, 16)
+    #saveStats(0, "ana", 200, 21)
+    #saveStats(1, "didi", 10, 12)
+    print(getStats(0))
